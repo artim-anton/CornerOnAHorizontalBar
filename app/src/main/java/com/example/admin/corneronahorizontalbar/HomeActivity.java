00a0c45f40400a6a7c25de59ceee980131c96f13
artim-anton <artim-anton@yandex.ru>
2017-10-25 23:10:43 +0300

    sound

--- app/src/main/java/com/example/admin/corneronahorizontalbar/HomeActivity.java
+++ app/src/main/java/com/example/admin/corneronahorizontalbar/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.admin.corneronahorizontalbar;
 
+import android.annotation.TargetApi;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +12,7 @@ import android.view.View;
 import android.widget.Chronometer;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements SoundPool.OnLoadCompleteListener {
 
     private Chronometer mChronometer;
     private int sound;
@@ -19,34 +23,69 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createSoundPoolWithBuilder();
+        } else {
+            createSoundPoolWithConstructor();
+        }
+
+        soundPool.setOnLoadCompleteListener(this);
+        sound = soundPool.load(this, R.raw.click, 1);
+
         mChronometer = (Chronometer) findViewById(R.id.chronometer);
 
-        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+/*        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
                 long elapsedMillis = SystemClock.elapsedRealtime()
                         - mChronometer.getBase();
 
-                /*if (elapsedMillis > 5000) {
+                if (elapsedMillis > 5000) {
                     String strElapsedMillis = "Прошло больше 5 секунд";
                     Toast.makeText(getApplicationContext(),
                             strElapsedMillis, Toast.LENGTH_SHORT)
                             .show();
-                }*/
+
             }
-        });
+        });*/
     }
 
    public void onStartClick(View view) {
+        soundPool.play(sound, 1, 1, 0, 0, 1);
         mChronometer.setBase(SystemClock.elapsedRealtime());
         mChronometer.start();
     }
 
     public void onStopClick(View view) {
+        soundPool.play(sound, 1, 1, 0, 0, 1);
         mChronometer.stop();
     }
 
     public void onResetClick(View view) {
+        soundPool.play(sound, 1, 1, 0, 0, 1);
         mChronometer.setBase(SystemClock.elapsedRealtime());
+    }
+
+
+    // sound
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void createSoundPoolWithBuilder() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder().setAudioAttributes(attributes).setMaxStreams(1).build();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void createSoundPoolWithConstructor() {
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+    }
+
+
+    @Override
+    public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+
     }
 }
